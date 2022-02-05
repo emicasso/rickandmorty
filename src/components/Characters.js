a439e28bb530e02663737af788a006fd8adf9407
Emiliano Cassoni <cassoniemiliano@gmail.com>
2022-02-04 21:11:21 -0300

    Mejora estetica

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -4,7 +4,7 @@ const Characters = ({ characters = [] }) => {
   return (
     <div className="row">
       {characters.map((item, index) => (
-        <div key={index} className="col">
+        <div key={index} className="col-sm-3 my-3">
           <div className="card" style={{minWidth: "200px"}}>
             <img src={item.image} alt="" />
             <div className="card-body">
